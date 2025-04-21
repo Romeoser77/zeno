505a7b73a8bd3c142a3fbf827801bcdf0377eb08
Romeoser77 <romeomods26@gmail.com>
2025-04-21 13:39:26 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+
+// USE ENVIRONMENT VARIABLES FOR EMAIL AND PASSWORD INSTEAD.
+module.exports = {
+  PREFIX: process.env.PREFIX || "zenomd×",
+  EMAIL: process.env.EMAIL || "ajithjithu8714@gmail.com",
+  PASS: process.env.PASS || "ARM+a0GWOuio#5",
+  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
+  IMAGE: process.env.IMAGE || "https://files.catbox.moe/xbog35.jpeg",
+}
